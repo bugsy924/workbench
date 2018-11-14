3e7d483da6efd3e2012bc555f8790bc094812183
Bugsy <30946888+bugsy924@users.noreply.github.com>
2018-11-14 20:34:11 +1100

    Words

--- app/components/commands/get_fingerprint.js
+++ app/components/commands/get_fingerprint.js
@@ -5,7 +5,7 @@ class GetFingerprint extends PureComponent {
   constructor(props) {
     super(props);
     this.name = "Get Fingerprint";
-    this.description = "Openpilot needs to successfully match the car's CAN messages.  This is called the fingerprint.  Executing this command can help you figure out if Openpilot sees your vehicle.";
+    this.description = "Openpilot identifies your car by using a list of its CAN messages.  This is called a fingerprint.  Executing this command while the car is on retreives the fingerprint of your car.";
     this.commands = [
       "cd /data/openpilot/selfdrive",
       "PYTHONPATH=/data/openpilot PREPAREONLY=1 /data/openpilot/selfdrive/debug/get_fingerprint.py"
